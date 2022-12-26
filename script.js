2f422d8df0f5bf4e1959af08e180239bdc6418f7
Fabiana  Campanari <fabicampanari@gmail.com>
2022-12-26 02:35:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const listaYT = [
   "https://yt3.ggpht.com/ytc/AMLnZu9fwc9E-G17B2vMnbYzZBp_NYs4AmCE802y8x08=s88-c-k-c0x00ffffff-no-rj",
   "https://yt3.ggpht.com/ytc/AMLnZu_3V_iTCz3_20NZc0_WypBbS1AK3QsBkJF001k8SQ=s88-c-k-c0x00ffffff-no-rj",
   "https://yt3.ggpht.com/ytc/AMLnZu8zAmF1BDo7p8K9xw_3tr0J8fEdWbbq64ykTRSTpg=s88-c-k-c0x00ffffff-no-rj",
+
   "https://yt3.ggpht.com/ytc/AMLnZu8lUVeJ1TiYevFOzZPNQFEQSDHc-TwiYKMcQpxpvA=s88-c-k-c0x00ffffff-no-rj",
+
   "https://yt3.ggpht.com/ytc/AMLnZu9XCumF881WgJ5yEIZp7BWuImKNC9T-IwVUEGeXyg=s88-c-k-c0x00ffffff-no-rj",
   "https://yt3.ggpht.com/ytc/AMLnZu_8E2fVaNH2D20xn32ph9axohUCgWaLpXO_tXJ14g=s88-c-k-c0x00ffffff-no-rj",
   "https://yt3.ggpht.com/ytc/AMLnZu9SFJaGUnZnhHBfmnrSQ_5IZObGBhia1VSMVdpTEg=s88-c-k-c0x00ffffff-no-rj",
@@ -12,18 +14,29 @@ const listaYT = [
 
   "https://yt3.ggpht.com/Fqr4oo0VR4l5KB2E7frxX00reaPw3oBX5csxGmijKx5UTDPY4xLPW0eYTnzh5TvkqNV35g-HHQ=s88-c-k-c0x00ffffff-no-rj",
 
-  "https://yt3.ggpht.com/d0FgDK1WsvuD3iikgwSZpaqxnEmd8E4-xNoCMtJvOINujkySmwB3CEzL9Xc4KenbP6i_qgvEcg=s88-c-k-c0x00ffffff-no-rj",
+  "https://yt3.ggpht.com/_zaUyr6nSjroD818AhkDuwBGIy5tr_DKmddob8RkeuK-nmN9CQLXb2ayAeBK2_Ojnb2Cd7uSC5I=s88-c-k-c0x00ffffff-no-rj",
+
+  "https://yt3.googleusercontent.com/YtEFxzWdIN8cMOFSNXacDlVZFzE8k_K6Xn_bOc_ehh2q49YGQ-QmwS-fAg_rJeM7GnTldac7=s88-c-k-c0x00ffffff-no-rj",
+
+  "https://yt3.googleusercontent.com/ytc/AMLnZu8sKqJsWutKOkODqApDfyKFjQ3eb3hqL89MJt7y=s88-c-k-c0x00ffffff-no-rj",
+
   "https://yt3.ggpht.com/m8Yrqf0Y81L4mZ1Cq7rGnJ4NDHe6729rxiFZvWyy9-xd_Z7Osmza7J8NWpN1axMHcCb1oKJNgA=s88-c-k-c0x00ffffff-no-rj",
+
   "https://yt3.ggpht.com/ytc/AMLnZu8ecQorMO_sToYiqZJynYBeaz0Xa_HK-Z6UA4aO=s88-c-k-c0x00ffffff-no-rj",
   "https://yt3.ggpht.com/ytc/AMLnZu_2b9piwsef9cub_jjHPcMNw8JNAXNKmf3kQpSB=s88-c-k-c0x00ffffff-no-rj",
   "https://yt3.ggpht.com/ytc/AMLnZu9-n9mDtKOwVMx3_-2zr8H57lHFag5t-0JcJNFPUQ=s88-c-k-c0x00ffffff-no-rj",
   "https://yt3.ggpht.com/ytc/AMLnZu9ZwnwHt4DaY03iQD1mALpJKFPbMBiilsc1KRCV5g=s88-c-k-c0x00ffffff-no-rj",
   "https://yt3.ggpht.com/ytc/AMLnZu_B2N15Iv2b8IYDYcrjkXScMam9LGnVyfadI_Fu1ok=s88-c-k-c0x00ffffff-no-rj",
   "https://yt3.ggpht.com/z0Ccoolotr1Q4F2EaCIgaR4GWLAR_VCwP25QrTica_e9XV6x8-kuQx_qnqI1mnaOkYl6KkIL=s88-c-k-c0x00ffffff-no-rj",
+
   "https://yt3.ggpht.com/84ALFuw4UCyfL1TyoMU77D-I6xngPjn2X0kbx2bMAyoFzWwR-utcrbDn_MuNJcroPcmU9NdGGQ=s88-c-k-c0x00ffffff-no-rj",
+
   "https://yt3.ggpht.com/ytc/AMLnZu-rihMwxuXoZ2IjdwvfXegytnGVYquLdSBhRf5-ZOM=s88-c-k-c0x00ffffff-no-rj",
+
   "https://yt3.ggpht.com/ytc/AMLnZu-wtZzoT6Qpw-h2Ng_bpT8SvaWca_w-EGKc5D69kQ=s88-c-k-c0x00ffffff-no-rj",
+
   "https://yt3.ggpht.com/ytc/AMLnZu-50eV3fK6unoZyi0yLMWnAo-NIRRy1PMTPUen8=s88-c-k-c0x00ffffff-no-rj",
+
   "https://yt3.ggpht.com/_q52i8bUAEvcb7JR4e-eNTv23y2A_wg5sCz0NC0GrGtcw1CRMWJSOPVHUDh_bngD0q4gMvVeoA=s88-c-k-c0x00ffffff-no-rj",
   "https://yt3.ggpht.com/ytc/AMLnZu9UJxaTaTBMLMbZA1s14YCYhFkZjXGHTU8pD5M5=s88-c-k-c0x00ffffff-no-rj",
   "https://yt3.ggpht.com/ytc/AMLnZu9WFZ1kTBuy0mo3cVlo17QFXNT_55y2W_QMoRWLFw=s88-c-k-c0x00ffffff-no-rj"
@@ -41,6 +54,8 @@ const listaLink = [
   "https://www.youtube.com/c/DevmediaBrasil",
   "https://www.youtube.com/c/Alura",
   "https://www.youtube.com/c/Chiefofdesign",
+  "https://www.youtube.com/@attekitadev",
+  "https://www.youtube.com/@42saopaulo54",
   "https://www.youtube.com/c/DevSoutinho",
   "https://www.youtube.com/c/WagnerAguiar",
   "https://www.youtube.com/c/Programa%C3%A7%C3%A3oDin%C3%A2mica",
@@ -67,6 +82,8 @@ const listaNome = [
   "DevMedia",
   "Alura",
   "Chief of Design",
+  "Attekita Dev",
+  "42 São Paulo",
   "Mario Souto - Dev Soutinho",
   "Wagner Aguiar",
   "Programação Dinâmica",
